4865d70f04150fa5c93e5a9cd1277c65cd0313d2
Bazaley <88235404+Bazaley@users.noreply.github.com>
2022-10-01 16:59:50 +0000

    Deploying to gh-pages from @ Bazaley/Dashboard@76fc37d7bb057e08e08e13f411c0d005ff95a175 🚀

---
+++ index.28bcf51d.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".side-menu__btn"),s=document.querySelector(".side-menu"),n=document.querySelector(".side-menu__btn-close");e.addEventListener("click",(function(){s.classList.add("is-open"),e.classList.add("is-hidden")})),n.addEventListener("click",(function(){s.classList.remove("is-open"),e.classList.remove("is-hidden")}));
+//# sourceMappingURL=index.28bcf51d.js.map
---
+++ index.daa159fd.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".side-menu__btn"),n=document.querySelector(".side-menu"),s=document.querySelector(".side-menu__btn-close");e.addEventListener("click",(function(){n.classList.add("is-open"),e.classList.add("is-hidden")})),s.addEventListener("click",(function(){n.classList.remove("is-open"),e.classList.remove("is-hidden")}))}();
+//# sourceMappingURL=index.daa159fd.js.map
